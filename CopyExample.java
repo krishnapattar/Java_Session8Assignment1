5d52ffabc30c7c8215a4348e4d4cbf82f0bc130a
krishnapattar <krishnapattar140@gmail.com>
2017-08-05 18:45:47 +0530

    Add files via upload

---
+++ CopyExample.java
@@ -0,0 +1,44 @@
+package ses8asi3;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+ 
+/*
+ * Write a program for file copy.
+ */
+public class CopyExample //proper naming conventions is used 
+{
+    public static void main(String[] args)// Main methods starts
+    {	
+    	FileInputStream instream = null;
+	FileOutputStream outstream = null;
+ 
+    	try{
+    	    File infile =new File("D:\\File1.txt");
+    	    File outfile =new File("D:\\File2.txt");
+ 
+    	    instream = new FileInputStream(infile);
+    	    outstream = new FileOutputStream(outfile);
+ 
+    	    byte[] buffer = new byte[1024];
+ 
+    	    int length;
+    	    /*copying the contents from input stream to
+    	     * output stream using read and write methods
+    	     */
+    	    while ((length = instream.read(buffer)) > 0){
+    	    	outstream.write(buffer, 0, length);
+    	    }
+
+    	    //Closing the input/output file streams
+    	    instream.close();
+    	    outstream.close();
+
+    	    System.out.println("File copied successfully!!");
+ 
+    	}catch(IOException ioe){
+    		ioe.printStackTrace();
+    	 }
+    }
+}// class closed
